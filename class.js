6204dc3dc870c5f0aeaa1ae7e110922d2442da20
MD: Raj Ali Sheikh <138352243+rajalishikh@users.noreply.github.com>
2026-02-07 12:32:08 +0600

    Now I am Understood the concept class which is so much usefull

--- class.js
+++ class.js
@@ -1,9 +1,17 @@
 class Plyer1{
+    constructor(name,age){
+       this.name=name;
+       this.age=age;
+       this.nation="Bangladeshi"
+    }
 
     goal(){
         console.log("I am not a good Player")
     }
+    aim(){
+        return "Don't waste your money in the wrong way"
+    }
 } 
-const Player_value=new Plyer1()
-Player_value.goal()
-console.log(Player_value)
+const Player_value=new Plyer1("Raj","26")
+const Player_value2=new Plyer1("Arif","25")
+console.log(Player_value2)
