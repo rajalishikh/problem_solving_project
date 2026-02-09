d1d827959d084410aa137871953a42a04d4f5d08
MD: Raj Ali Sheikh <138352243+rajalishikh@users.noreply.github.com>
2026-02-09 11:30:34 +0600

    I am convert the data into stringify

--- string.js
+++ string.js
@@ -7,7 +7,8 @@ const JsonFamily={
     I_middle_class:true,
     father_Is_a_best_thing_In_Life:true,
 }
-
 console.log(JsonFamily)
 const convert_json_stringfy=JSON.stringify(JsonFamily)
-console.log(convert_json_stringfy)
+console.log("convert this data in stringify",convert_json_stringfy)
+const convert_parse_Data=JSON.parse(convert_json_stringfy)
+console.log("Convert The data in to parse", convert_parse_Data)
