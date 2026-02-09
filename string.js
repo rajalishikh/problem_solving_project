fed15778e247cdd03f2e4ac675fff9890165fe2b
MD: Raj Ali Sheikh <138352243+rajalishikh@users.noreply.github.com>
2026-02-09 11:15:12 +0600

    I am convert data in json stringyfi

---
+++ string.js
@@ -0,0 +1,13 @@
+const JsonFamily={
+    wife:"Coast of maintain so much High",
+    family:"medium high",
+    sonshar_coast:"13750",
+    pocket_money:"1200",
+    I_am_rich:false,
+    I_middle_class:true,
+    father_Is_a_best_thing_In_Life:true,
+}
+
+console.log(JsonFamily)
+const convert_json_stringfy=JSON.stringify(JsonFamily)
+console.log(convert_json_stringfy)
