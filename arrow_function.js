634b135671314eeabc80f79063df20fe376684e2
MD: Raj Ali Sheikh <138352243+rajalishikh@users.noreply.github.com>
2026-02-21 15:48:56 +0600

    Understand of or operation

---
+++ arrow_function.js
@@ -0,0 +1,6 @@
+const value=false;
+const showUser=()=>console.log("Show User")
+const hideUser=()=>console.log("hide user ")
+
+value && showUser() 
+value || hideUser()
--- just_recup_topic.js
+++ just_recup_topic.js
@@ -1,6 +1,5 @@
 let my_hero_name=["SRk","Salman khan film ","RoberDouny_junior"]
 
-
 for(let index=0;index<my_hero_name.length;index++){
     console.log(my_hero_name[index])
 
